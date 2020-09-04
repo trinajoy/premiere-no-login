13fc1f002299616a76f51929b3d2c1c32af34565
Katrina H <katrina.hitchcock@gmail.com>
2020-09-04 12:06:12 +0200

    update database config

--- includes/config.php
+++ includes/config.php
@@ -4,11 +4,11 @@ session_start();
 date_default_timezone_set("Europe/London");
 
 try {
-    $con = new PDO("mysql:dbname=guest;host=mypremiereserver.mysql.database.azure.com", "myadmin@mypremiereserver", "Password123");
+  
+    // $con = new PDO('mysql:dbname=premiere;host=localhost', 'root', '');
+    $db = new PDO('mysql:host=premiereserver.mysql.database.azure.com;port=3306;dbname=test', 'myadmin@premiereserver', 'Password123', $options);
+
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
     exit('Connection failed:' . $e->getMessage());
 }
-
-
-//$con = new PDO('mysql:dbname=premiere;host=localhost', 'root', '');
