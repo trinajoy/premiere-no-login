e89e98d5358e428da51f465b3397e5dacf00a64c
Katrina H <katrina.hitchcock@gmail.com>
2020-09-11 22:20:58 +0200

    remove duplicate videos

--- includes/config.php
+++ includes/config.php
@@ -5,9 +5,9 @@ date_default_timezone_set("Europe/London");
 
 try {
   
-    $con = new PDO('mysql:dbname=premiere;host=localhost', 'root', '');
+    // $con = new PDO('mysql:dbname=premiere;host=localhost', 'root', '');
 
-    // $con = new PDO('mysql:dbname=heroku_6256616d7ff11ea;host=eu-cdbr-west-03.cleardb.net', 'bbeafe52c7b661', '80c9e66b'); 
+    $con = new PDO('mysql:dbname=heroku_6256616d7ff11ea;host=eu-cdbr-west-03.cleardb.net', 'bbeafe52c7b661', '80c9e66b'); 
 
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
