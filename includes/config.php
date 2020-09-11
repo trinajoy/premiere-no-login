d46d13e82e5fbfd0c8d67a8a2acbc52c6c8cf956
Katrina H <katrina.hitchcock@gmail.com>
2020-09-11 19:07:26 +0200

    update azure config

--- includes/config.php
+++ includes/config.php
@@ -6,10 +6,12 @@ date_default_timezone_set("Europe/London");
 try {
   
 
-    $con = new PDO('mysql:dbname=heroku_6256616d7ff11ea;host=eu-cdbr-west-03.cleardb.net', 'bbeafe52c7b661', '80c9e66b');
+    //$con = new PDO('mysql:dbname=heroku_6256616d7ff11ea;host=eu-cdbr-west-03.cleardb.net', 'bbeafe52c7b661', '80c9e66b');
     
     // $con = new PDO('mysql:dbname=premiere;host=localhost', 'root', '');
-   // $db = new PDO('mysql:host=trinatestserver.mysql.database.azure.com;port=3306;dbname=testdb', 'myadmin@trinatestserver', 'Password123', $options);
+    $con = new PDO('mysql:host=trinatestserver.mysql.database.azure.com;port=3306;dbname=testdb', 'myadmin@trinatestserver', 'Password123');
+
+    // $con = new PDO('mysql:host=trinatestserver.mysql.database.azure.com;port=3306;dbname=testdb', 'myadmin@trinatestserver', 'Password123', $options);
 
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
